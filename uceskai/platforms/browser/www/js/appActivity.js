c140c5f201f09dc3ad0d642109fb19ad1187f4c3
mkainz <uceskai@ucl.ac.uk>
2018-05-08 13:34:22 -0400

    added question display

--- uceskai/platforms/browser/www/js/appActivity.js
+++ uceskai/platforms/browser/www/js/appActivity.js
@@ -5,3 +5,29 @@ function menuClicked() {
 function replaceGraphs() {
 			document.getElementByld("graphdiv").innerHTML="<img src=' images/ucl.png'>"
 }
+
+
+
+// track user location. Code sourced from: https://stackoverflow.com/questions/10563789/how-to-locate-user-with-leaflet-locate
+function trackLocation(){
+mymap.locate({setView: true, watch: true}) /* This will return map so you can do chaining */
+        .on('locationfound', function(e){
+            var marker = L.marker([e.latitude, e.longitude]).bindPopup('Your are here').openPopup();
+            var circle = L.circle([e.latitude, e.longitude], e.accuracy/80, {
+                weight: 1,
+                color: 'blue',
+                fillColor: '#cacaca',
+                fillOpacity: 0.2
+            });
+            mymap.addLayer(marker);
+            mymap.addLayer(circle);
+        })
+       .on('locationerror', function(e){
+            console.log(e);
+            alert("Location access denied.");
+        });
+		
+
+};
+
+
