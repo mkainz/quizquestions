83cceed9bb052cc2aab252534f14fa8ea8ac5ce9
mkainz <uceskai@ucl.ac.uk>
2018-05-08 13:44:14 -0400

    final questions app

--- uceskai/platforms/browser/www/js/uploadData.js
+++ uceskai/platforms/browser/www/js/uploadData.js
@@ -1,26 +1,14 @@
 function startDataUpload() {
 	alert ("start data upload");
 
-	var question = "'Head Question'";
+	var loc = document.getElementById("location").value;
+	var question = document.getElementById("question").value;
 	var correct = document.getElementById("correct").value;
 	var answer1 = document.getElementById("answer1").value;
 	var answer2 = document.getElementById("answer2").value;
 	var answer3 = document.getElementById("answer3").value;
 	
-	var postString = "&question= Whose head in main cloisters?"+question + "&correct="+correct +"&answer1="+answer1 +"&answer2="+answer2 +"&answer3="+answer3;
-	//get answer (radio buttons)
-	if (document.getElementById("answer1").checked) {
-		postString=postString+"&answer=Jeremy Bentham";
-	}
-	if (document.getElementById("answer2").checked) {
-		postString=postString+"&answer=Elton John";
-	}	
-	if (document.getElementById("answer3").checked) {
-		postString=postString+"&answer=A prehistoric opposum";
-	}	
-	if (document.getElementById("answer4").checked) {
-		postString=postString+"&answer=Joe Tribbiani";
-	}	
+	var postString = "location="+loc +"&question="+question + "&correct="+correct +"&answer1="+answer1 +"&answer2="+answer2 +"&answer3="+answer3;
 	processData(postString);
 	
 	alert (postString);
@@ -29,7 +17,7 @@ function startDataUpload() {
 var client;
 function processData(postString) {
 	client = new XMLHttpRequest();
-	client.open('POST','http://developer.cege.ucl.ac.uk:30299/submitQuestion',true);
+	client.open('POST','http://developer.cege.ucl.ac.uk:30299/uploadData',true);
 	client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	client.onreadystatechange = dataUploaded;
 	client.send(postString);
